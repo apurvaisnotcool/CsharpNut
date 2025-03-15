643ab207a38d4b5b6d658147ccaa863dcb3840dd
kira <113971943+apurvaisnotcool@users.noreply.github.com>
2025-03-15 06:42:17 +0530

    Add files via upload

---
+++ 2Parsing/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello, World!");
+// I want soberity for you.
+// This is a hard task but i am going to complete it 
+// I will get sober and i will learn this language.
+// if you are reading this it is not too late you can get sober too.
+/* Okay so unity. It is a tough journey.
+*/
+
+// How to create variable
+
+//datatype variable_name = initialization
+
+int i = 10; // Example
+System.Console.WriteLine("My number "+i);
+System.Console.WriteLine("Hello Kira");
+
+string userInput = Console.ReadLine(); //Console.ReadLine() will get us a string 
+//therefore entering an int lead to an error
+
+System.Console.WriteLine("The User Input is "+userInput);
+
+
+//Parsing is a way to convert data from one datatype to another data type
+int myNumber = 0;
+myNumber = int.Parse(Console.ReadLine()); //we Run into error if we don't parse the string
+
+System.Console.WriteLine("The number that was enterered as a string is "+myNumber);
+//To get the output
+
+//In this line we Convert the String input from Console.ReadLine() and convert it to Integer
+
+
+float myFloat;
+
+myFloat = float.Parse(Console.ReadLine());
+System.Console.WriteLine("Please enter a float");
+System.Console.WriteLine(myFloat);
+
+
+
