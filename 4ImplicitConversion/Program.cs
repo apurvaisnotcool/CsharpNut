8e5fc2b72ba43e6daa26b2a1a5ad3698443bf0d1
kira <113971943+apurvaisnotcool@users.noreply.github.com>
2025-03-15 06:44:56 +0530

    Add files via upload

---
+++ 4ImplicitConversion/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Implicit Conversion is a way in which we can Convert a variable from one data type to another
+as long as the byte size of the new variable datatype is more than the old variable datatype
+
+for ex:
+
+int can be converted into a double or a long
+*/
+
+int myInt = 10;
+long myLong = myInt;//myInt can be assigned to myLong because long holds more bytes and bits than int
+System.Console.WriteLine($"Integer after being converted to long is {myLong}");
---
+++ 5ExplicitConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿//explicit Conversion
+
+long myLong = 12345;
+
+int myInt = (int)myLong;
+
+System.Console.WriteLine(myInt);
+
+
+/*
+keep in mind that long holds more value than int so if we put a large long number and explicitly convert
+it into an int we might get a rubbish answer
+ex:
+*/
+long myLong2 = 12345678901023485;
+int i = (int)myLong2;
+System.Console.WriteLine(i);//output is not the number stored in myLong2
+
+
+int j;
+double myDouble =13.5;
+
+j=(int)myDouble;//Everything after the decimal point will be cutoff
+
+System.Console.WriteLine($"J value will be {j} not '13.5' because we are casting double to an int");
+
