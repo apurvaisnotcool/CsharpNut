6baec16ef737dd7ac954dcd0f05cf645ed4fa3fd
kira <113971943+apurvaisnotcool@users.noreply.github.com>
2025-08-29 01:28:49 +0530

    Add files via upload

---
+++ 29StartingOOP/Program.cs
@@ -0,0 +1,9 @@
+﻿/*From Here We are Starting Object Oriented Programming
+This file just Exist to Divide the Notes in Different Parts.
+After this the Course Gets tougher and i get Sober
+This is the section two of the course
+*/
+
+/*
+I hope These Notes Help Students Become Better Coders
+*/
