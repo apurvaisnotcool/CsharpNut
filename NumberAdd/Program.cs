8f8895f6087c8dc3144740da5779296fbdcc61d1
kira <113971943+apurvaisnotcool@users.noreply.github.com>
2025-08-30 01:33:16 +0530

    Add files via upload

---
+++ NumberAdd/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NumberAdd;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalValue = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            int value = int.Parse(Console.ReadLine());
+            totalValue += value;
+        }
+
+        System.Console.WriteLine(totalValue);
+            
+    }
+}
