c807ff95aa599f56a0aa04a4bcab0fe09250555b
kira <113971943+apurvaisnotcool@users.noreply.github.com>
2025-08-31 00:59:39 +0530

    Add files via upload

---
+++ 33Inheritance/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _33Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat("GreyHound");//We can access the Eat() Function from the Dog class because Dog Inherits 
+                             // from Animals
+        GoldenRetriver goldenRetriver = new GoldenRetriver();
+        //This Object Inherits from Dog Class which in then Inherits from the Animal Class
+        //Theirfore we can access the Dog Methods and properties as well as the Animal 
+        // Methods and properties
+        goldenRetriver.Name = "GolddenRetriver";
+        goldenRetriver.Eat(goldenRetriver.Name);
+        goldenRetriver.Bark();
+        Cat cat = new Cat();
+        cat.Meow();
+        dog.Food();//Since we are calling the Food Function from the dog object we get Dog Food
+        cat.Food();//Since we are calling the Food Function from the cat object we get Cat Food
+        //This is an Example of Polymorphism
+    }
+}
+
+
+//Base Class(parent class or Super Class)
+class Animal
+{
+    public void Eat(string name)
+    {
+        Console.WriteLine($"{name} is Eating...");
+    }
+
+    //Virtual Keyword helps us override the Food Function in Other Derived classes
+    public virtual void Food()
+    {
+        System.Console.WriteLine("Food for Animal");
+    }
+}
+
+//This Dog Class Inherits from Animal Class
+//Dog has all the capabilities that Animal Class has plus the properties and methods of its own
+//Class
+
+//Dog is a Derived Class(Sub class or Child Class)
+class Dog : Animal
+{
+    public void Bark()
+    {
+        System.Console.WriteLine("Barking...");
+    }
+
+    public override void Food()
+    {   //base keyword helps use call the base funtion food from the animal class
+        base.Food();
+        System.Console.WriteLine("Food for Dogs");
+    }
+}
+
+//This is Hierical inheritance where we Derive multiple classes from the base class in this case
+//Animal class
+class Cat : Animal
+{
+    public void Meow()
+    {
+        System.Console.WriteLine("Cat is Meowing");
+    }
+
+    public override void Food()
+    {
+        System.Console.WriteLine("Food for Cats");
+    }
+}
+
+class GoldenRetriver : Dog
+{
+    private string _name = "";
+    public string Name { get => _name; set => _name = value; }
+
+    public void Active()
+    {
+        System.Console.WriteLine("Active Dog");
+    }
+}
---
+++ 34Inheritance2/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace _34Inheritance2;
+
+class Program
+{
+    /* Constructor Inheritance:
+    Constructors are special methods in a class that are called when an instance of the class is created.
+    So how does that matter in the context of inheritance?
+    constructors of the base class are called before the constructor of the derived class is called.
+    This ensures that the base class is properly initialized before any additional initialization in the
+    derived class takes place.*/
+
+    /*
+    Proper Initialization: This Ensures that all Fields and Properties of the base class are Correctly
+    Setup before any Operation of the Derived class can take place
+    This means that when creating an Object of the Derived class the constructor of the base class
+    runs first to Initialize its Members
+    */
+
+    /*
+    Consistent State: This helps maintain a consistent and valid state of the Object heirarchy
+    This ensures that both the base and derived class remain in the valid state throughout the objects
+    lifetime.By running the base class constructor first we ensure that any dependency and required 
+    initial state are estabilished.
+    */
+
+    /*
+    Reuse of Initialization code:
+    This avoids the duplication of the Initialization code by Reusing the base class Constructor
+    */
+    static void Main(string[] args)
+    {
+        Employee kira = new Employee("Kira", 27, "Coder", 666);
+        /*
+        Here we are creating an Object named Kira
+        when the object Employee kira is created we call the Person Constructor
+        Than the Employee Constructor is Called 
+        */
+        kira.DisplayPersonInfo();
+        //Here the Kira Employee calls the DisplayPersonInfo Function which has the Assigned Name 
+        //and Age
+        Manager carl = new Manager("carl", 36, "Manager", 123, 7);
+        carl.DisplayManagerInfo();
+    }
+
+}
+    public class Person
+    {
+        public string Name { get; private set; }//we can get the name but noone can set it
+        public int Age { get; private set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            System.Console.WriteLine("Person Constructor called");
+        }
+        public void DisplayPersonInfo()
+        {
+        System.Console.WriteLine
+            ($"Name: {Name} Age: {Age}");
+        }
+
+        
+    }
+public class Employee : Person
+{
+    public string JobTitle { get; private set; }
+    public int EmployeeID { get; private set; }
+    public Employee(string name, int age, string jobTitle, int employeeId) : base(name, age)
+
+    {
+        JobTitle = jobTitle;
+        EmployeeID = employeeId;
+        System.Console.WriteLine("Employee Constructor is called");
+    }
+    public void DisplayEmployeeInfo()
+    {
+        DisplayPersonInfo();
+        System.Console.WriteLine($"JobTitle: {JobTitle}, EmployeeId: {EmployeeID}");
+    }
+    
+}
+
+//Employee Class Inherits the Properties and Methods of the 
+// Person Class(Name, Age, DisplayPersonInfo())
+//we have to call the base properties from the parent class 
+//This will assign the name and age taken from the Employee Constructor to the Base Constructor
+
+//name and age in the Employee Constructor is passed on to the base
+/*
+When we create the Employee Object Kira we pass in the value from that object
+to the Employee Constructor that gets passed on to the base constructor Person
+that gets assigned to the Name and Age property
+*/
+public class Manager : Employee
+{
+    public int TeamSize { get; private set; }
+    public Manager(string name, int age, string jobTitle, int employeeID, int teamSize)
+    : base(name, age, jobTitle, employeeID)
+    {
+        TeamSize = teamSize;
+        System.Console.WriteLine("Manager Constructor is called");
+    }
+    public void DisplayManagerInfo()
+    {
+        DisplayEmployeeInfo();
+        System.Console.WriteLine($"TeamSize: {TeamSize}");
+    }
+}
+
+
